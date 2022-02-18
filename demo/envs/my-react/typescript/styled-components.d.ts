f87ca3e7822e26dd7862af8cd2071e6d12deb0b0
Thomas Moore <thomasmoore@Thomass-MacBook-Pro.local>
2022-02-18 10:31:00 +0000

    remove demo

--- demo/envs/my-react/typescript/styled-components.d.ts
+++
@@ -1,7 +0,0 @@
-import { ExportedDefaultTheme } from '@tommoore96/provider-example.themes';
-
-import 'styled-components';
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends ExportedDefaultTheme {}
-}
